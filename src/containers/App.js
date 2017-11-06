d4756799c7c8345cc6fc11d971ced5e2f008a336
esirK <esirkings@gmail.com>
2017-11-07 02:58:15 +0300

    Setup basic template to work with

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
