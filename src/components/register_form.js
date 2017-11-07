4116b61987a5d800b5257199df212b2eaa241a64
esirK <esirkings@gmail.com>
2017-11-08 02:20:48 +0300

    [CHORE] Style Registration form component using bootstrap

--- src/components/register_form.js
+++ src/components/register_form.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { registerUser } from '../actions/index';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {registerUser} from '../actions/index';
 
 
-class RegisterForm extends Component{
-    constructor(props){
+class RegisterForm extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             username: '',
@@ -14,67 +14,91 @@ class RegisterForm extends Component{
         };
         this.onFormSubmit = this.onFormSubmit.bind(this);
     }
-    render(){
-        return(
-            <form onSubmit={this.onFormSubmit}>
-                <div className="form-group">
-                    <div className="input-group input-group-lg">
-                        <div className="input-group-addon">
-                            <span className="icon-mail"></span>
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-sm-6 col-sm-offset-3">
+                    <div className="shoppinglist-dialog">
+                        <h1>Shopping List </h1>
+                        <div className="dialog-body">
+                            <p>
+                                Enter your details as asked below in order to join the Shopping list app.
+                            </p>
                         </div>
-                        <input
-                            type="text"
-                            placeholder="Username"
-                            className="form-control"
-                            id='inputUsername'
-                            value={this.state.username}
-                        />
+                        <form onSubmit={this.onFormSubmit}>
+                            <div className="form-group">
+                                <div className="input-group input-group-lg">
+                                    <div className="input-group-addon">
+                                        <span className="icon-mail"></span>
+                                    </div>
+                                    <input
+                                        type="text"
+                                        placeholder="Username"
+                                        className="form-control"
+                                        id='inputUsername'
+                                        name="username"
+                                        value={this.state.username}
+                                    />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <div className="input-group input-group-lg">
+                                    <div className="input-group-addon">
+                                        <span className="icon-mail"></span>
+                                    </div>
+                                    <input
+                                        className="form-control"
+                                        placeholder="example@example.com"
+                                        value={this.state.email}
+                                        type="email"
+                                        id='inputEmail'
+                                    />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <div className="input-group input-group-lg">
+                                    <div className="input-group-addon">
+                                        <span className="icon-dots"/>
+                                    </div>
+                                    <input
+                                        placeholder="Password"
+                                        value={this.state.password}
+                                        type="password"
+                                        className="form-control"
+                                    />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <div className="input-group input-group-lg">
+                                    <div className="input-group-addon">
+                                        <span className="icon-dots"/>
+                                    </div>
+                                    <input
+                                        placeholder="Confirm Password"
+                                        type="password"
+                                        className="form-control"
+                                    />
+                                </div>
+                            </div>
+                            <div className="clearfix">
+                                <button type="submit" className="btn btn-lg btn-primary pull-right"> Submit </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <div className="form-group">
-                    <div className="input-group input-group-lg">
-                    <label>Email</label>
-                    <input
-                        className="form-control"
-                        placeholder="example@example.com"
-                        value={this.state.email}
-                        type="email"
-                        id='inputEmail'
-                    />
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label>Password</label>
-                    <input
-                        placeholder="Password"
-                        value={this.state.password}
-                        type="password"
-                        className="form-control"
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Confirm Password</label>
-                    <input
-                        placeholder="Confirm Password"
-                        type="password"
-                        className="form-control"
-                    />
-                </div>
-                <label></label>
-                <span className="input-group-btn">
-					<button type="submit" className="btn btn-secondary"> Submit </button>
-				</span>
-            </form>
+            </div>
         );
     }
-    onFormSubmit(event){
+
+    onFormSubmit(event) {
         event.preventDefault();
         this.props.registerUser(this.state);
     }
 }
 
-function mapDispatchToProps(dispatch){
-    return bindActionCreators({ registerUser }, dispatch);
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({registerUser}, dispatch);
 }
 
 export default connect(null, mapDispatchToProps)(RegisterForm);
