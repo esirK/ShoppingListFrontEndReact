cfba640f1cd8ce60c9e9e9b91c9d130d134092fa
esirK <esirkings@gmail.com>
2017-11-08 00:28:19 +0300

    [CHORE] Refactor code structure

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
-
 import { BrowserRouter, Route } from 'react-router-dom'
 
 
+
 import reducers from './reducers';
+import RegisterForm from "./components/register_form";
 
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
-		<BrowserRouter>
-			<div>
-			<Route path="/register" component={App}/>
+        <BrowserRouter>
+            <div className="container">
+                <Route path="/register" component={RegisterForm}/>
             </div>
-		</BrowserRouter>
+        </BrowserRouter>
 	</Provider>
     , document.getElementById('root'));
