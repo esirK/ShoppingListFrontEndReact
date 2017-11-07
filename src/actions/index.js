d8ae2bd6bea794aa2959f8700f0094090cf8022d
esirK <esirkings@gmail.com>
2017-11-07 23:03:11 +0300

    [CHORE] Add action for the register user component

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+
+export const REGISTER_USER = 'REGISTER_USER';
+const URL = `http://127.0.0.1:5000/v1/register`;
+
+export function registerUser(details) {
+    console.log(details);
+    const request = axios.post(URL, {
+        "email": details['email'],
+        "name": details['name'],
+        "password": details['password']
+    });
+    return{
+        type: REGISTER_USER,
+        payload: request
+    };
+}
