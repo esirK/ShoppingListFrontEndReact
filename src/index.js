5b8bd2ebb62941cae068129ff8ccb91aa2b882d6
esirK <esirkings@gmail.com>
2017-11-16 03:31:01 +0300

    [FEATURE] Set user authorization for the whole app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+//import { composeWithDevTools } from 'redux-devtools-extension';
+
 import thunk from 'redux-thunk';
 import reduxPromise from 'redux-promise';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -15,7 +17,11 @@ import reducers from './reducers';
 import RegisterForm from './components/register';
 import LoginForm from './components/login_form';
 import ShoppingLists from './components/shoppinglists';
+
 import NotFound from './components/not_found';
+import {setAthorizationToken} from './components/helpers';
+import {setAuthStatusOfUser} from './actions';
+import jwt from 'jsonwebtoken';
 
 const createStoreWithMiddleware = applyMiddleware(reduxPromise, thunk)(createStore);
 
@@ -27,13 +33,19 @@ const muiTheme = getMuiTheme({
 		height: 100,
 	},
 });
+const store = createStoreWithMiddleware(reducers);
+if(localStorage.getItem('jwt')){
+	setAthorizationToken(localStorage.getItem('jwt'));
+	store.dispatch(setAuthStatusOfUser(jwt.decode(localStorage.getItem('jwt'))));
+}
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(reducers)}>
+	<Provider store={store}>
 		<BrowserRouter>
 			<MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
 				<AppBar title="Shopping List App" showMenuIconButton={false}/>
 				<div className="container">
 					<Switch>
+						<Route exact path="/" component={ShoppingLists}/>
 						<Route path="/login" component={LoginForm}/>
 						<Route path="/register" component={RegisterForm}/>
 						<Route path="/shoppinglists" component={ShoppingLists}/>
