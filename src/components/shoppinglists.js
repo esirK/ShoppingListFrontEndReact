27b17e99b2b8165f6ee8f6da38ded443b19b3c21
esirK <esirkings@gmail.com>
2017-11-17 12:22:38 +0300

    [FEATURE]Add shoppinglists components to display currently logged in user Shoppinglists

---
+++ src/components/shoppinglists.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+export default class ShoppingLists extends Component{
+	constructor(){
+		super();
+		this.state = {
+
+		};
+	}
+
+	render(){
+		return(
+			<div className='row'>
+				<p>I will display all shopping lists</p>
+			</div>
+		);
+	}
+}
