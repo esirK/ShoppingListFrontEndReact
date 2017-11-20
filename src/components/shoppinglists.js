49d9492759e885b421d5ccbed6237942a8eb5efa
esirK <esirkings@gmail.com>
2017-11-21 02:16:25 +0300

    Adds input dialog for users to enter new  shopping list info into

--- src/components/shoppinglists.js
+++ src/components/shoppinglists.js
@@ -3,20 +3,91 @@ import {Card, CardActions, CardHeader, CardText, FlatButton} from 'material-ui';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import {connect} from 'react-redux';
+
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
+
+
 import {addNewShoppingList, getShoppingLists} from '../actions';
 
 class ShoppingLists extends Component{
+	constructor(props){
+		super(props);
+		console.log('starting again');
+		this.state = {
+			addOpen: false,
+			name: '',
+			description: '',
+			error: props.error
+		};
+		this.handleFabClick = this.handleFabClick.bind(this);
+		this.handleClose = this.handleClose.bind(this);
+		this.handleTitleChange = this.handleTitleChange.bind(this);
+		this.handleDescChange = this.handleDescChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 	componentDidMount(){
-		console.log(this.props);
+		console.log('state ',this.state);
 		this.props.getShoppingLists();
 	}
-
+	handleFabClick(){
+		console.log('Clicked Fab', this.state.addOpen);
+		this.setState({ addOpen: true });
+	}
+	handleClose(){
+		this.setState({ name: '' });
+		this.setState({ description: ''});
+		this.setState({ addOpen: false });
+	}
+	handleSubmit(){
+		this.props.addNewShoppingList({
+			'name': this.state.name,
+			'description': this.state.description
+		});
+		this.setState({ name: '' });
+		this.setState({ description: ''});
+	}
+	handleTitleChange(e){
+		this.setState({ name: e.target.value });
+	}
+	handleDescChange(e){
+		this.setState({ description: e.target.value });
+	}
 	render(){
 		let cards = [];
-		if(this.props.error){
+		const actions = [
+			<FlatButton
+			  label="Cancel"
+			  primary={true}
+			  onClick={this.handleClose}
+			/>,
+			<FlatButton
+			  label="Submit"
+			  primary={true}
+			  onClick={this.handleSubmit}
+			/>,
+		  ];	
+		  const children = [
+			  <TextField
+      			hintText="Shoppinglist Name"
+				errorText={this.props.error}
+				value={this.state.name}
+				key='name'
+				onChange={this.handleTitleChange}
+			/>,
+			<TextField
+				hintText="Description of the shopping list"
+				multiLine={true}
+				rowsMax={4}
+				value={this.state.description}
+				key='desc'
+				onChange={this.handleDescChange}
+			/>
+		  ];  
+		if(this.state.error && !this.state.addOpen){
 			return(
 				<div id="cards">
-					<p>Got a {this.props.error} while Loading </p>
+					<p>Got a {this.props.error} while Loading Your Shoppinglists</p>
 				</div>
 			);
 		}
@@ -49,9 +120,16 @@ class ShoppingLists extends Component{
 			);
 			return(
 				<div id="cards">
+					<Dialog open={this.state.addOpen}
+						title="Add Shoppinglist"
+						actions={actions}
+						children={children}
+						autoDetectWindowHeight
+					>
+					</Dialog>
 			    {cards}
 					<div id="fab">
-						<FloatingActionButton secondary={true} onClick={this.props.addNewShoppingList}>
+						<FloatingActionButton secondary={true} onClick={this.handleFabClick}>
 							<ContentAdd />
 						</FloatingActionButton>
 					</div>
@@ -64,7 +142,7 @@ function mapStateToProps(state){
 	return {
 		isLoading: state.shoppinglists.isLoading,
 		shoppinglists: state.shoppinglists.shoppinglists,
-		error: state.shoppinglists.error
+		error: state.shoppinglists.error,
 	};
 }
 export default connect(mapStateToProps, {addNewShoppingList, getShoppingLists}) (ShoppingLists);
