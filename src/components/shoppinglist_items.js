7a85eaa296db18c8f8bc232e7253d20410b0bcc2
esirK <esirkings@gmail.com>
2017-12-04 22:05:48 +0300

    Adds card for displaying shoppinglist_items and loads them

--- src/components/shoppinglist_items.js
+++ src/components/shoppinglist_items.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { viewShoppingList } from '../actions/index';
+import { Card, CardHeader, CardText, CardActions, FlatButton } from 'material-ui';
 
 class ShoppingListItems extends Component{
 	constructor(props){
@@ -8,18 +10,69 @@ class ShoppingListItems extends Component{
 	}
 	componentDidMount(){
 		console.log('am at the Items Component');
+		this.props.viewShoppingList(this.props.match.params.id);
 	}
 	render(){
-		return(
-			<div>
-                Items {this.props.match.params.id}
-			</div>
-		);
+		let cards = [];
+		if(this.props.isLoading){
+			return(
+				<div>
+					<p>Loading...</p>
+				</div>
+			);
+		}
+		if(this.props.items.length === 0){
+			return(
+				<Card key='1'>
+					<CardHeader
+						title="No Items"
+					/>
+					<CardText>
+						You currently Have No Items on this Shoppinglist
+					</CardText>
+				</Card>
+			);
+		}
+		else{
+			this.props.items.map((item)=>
+				cards.push(
+					<Card key={item.id}
+						style={{
+							margin: '1% auto',
+						}}
+					>
+						<CardHeader
+							title={item.name}
+							style={{
+								textAlign: 'center',
+								'font-style': 'italic',
+								'font-weight': 'bold',
+							}}
+						/>
+						<CardText>
+							Price:{item.price}
+						</CardText>
+						<CardText>
+							Quantity:{item.quantity}
+						</CardText>
+						<CardActions>
+							<FlatButton label="Update" primary={true}/>
+							<FlatButton name='delete' label="Delete" secondary={true} />
+						</CardActions>
+					</Card>
+				));
+			return(
+				<div id="cards">
+                	{cards}
+				</div>
+			);
+		}
 	}
 }
 function mapStateToProps(state){
 	return {
 		items: state.shoppinglist_items.items,
+		isLoading: state.shoppinglist_items.isLoading
 	};
 }
-export default connect(mapStateToProps, null)(ShoppingListItems);
+export default connect(mapStateToProps, {viewShoppingList})(ShoppingListItems);
