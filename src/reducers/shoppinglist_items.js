ce0f3f69d598cb827d2f804f6847d79f1f1a1e5d
esirK <esirkings@gmail.com>
2017-12-04 22:14:18 +0300

    Add reducers for shoppinglist items loading and failed loading

--- src/reducers/shoppinglist_items.js
+++ src/reducers/shoppinglist_items.js
@@ -2,13 +2,23 @@ import * as types from '../constants/actiontypes';
 
 const initState = {
 	items: [],
+	isLoading: false,
+	error: ''
 };
 
 export default (state= initState, action)=>{
 	switch(action.type){
 	case types.SHOPPINGLIST_ITEMS_LOADED_SUCCESSFULLY:
 		return{
-			...state, items: action.items
+			...state, items: action.items, isLoading: false
+		};
+	case types.SHOPPINGLIST_ITEMS_LOADING:
+		return{
+			...state, isLoading: true
+		};
+	case types.SHOPPINGLIST_ITEMS_FAILED:
+		return{
+			...state, isLoading: false, error: action.error
 		};
 	default:
 		return state;
